c1576665fc3f40fdb53c5e40c41aa23cc37e1441
Peter Marshall <peter.marshall@uriam.com>
2019-03-25 19:37:48 +0000

    Tidied up the code and cleaned out some rubbish.

---
+++ senseGyro.py
@@ -0,0 +1,6 @@
+from sense_hat import SenseHat
+
+sense=SenseHat()
+
+while 1:
+    print (sense.gyroscope)
--- senseIt.py
+++ senseIt.py
@@ -2,10 +2,10 @@ from sense_hat import SenseHat
 from random import getrandbits
 from random import randint
 from datetime import datetime
-from time import sleep
 from kafka import KafkaProducer
 import subprocess
 import json
+import socket
 
 def pixelProgress(complete, total, fill, empty, reverse):
     if (complete > total):
@@ -25,23 +25,25 @@ def pixelProgress(complete, total, fill, empty, reverse):
         "pixelProgress : catastrophic miscalculation of the necessary pixel string length!")
     sense.set_pixels(M)
 
-def writeToJson(jsonData, filename):
-    try:
-        sense.show_letter('W')
-        with open(filename, 'w') as outfile:
-            json.dump(jsonData, outfile)
-	
-    except Exception as error:
-        print("Exception Thrown\n", repr(error))
 
-def sampleEnvironment(sensor, samples, decimalPoints, delaySeconds):
-    sumTemp = 0
+
+
+
+
+def sampleEnvironment(sensor, samples, decimalPoints):
     
+    # Variable setup
+    sumTemp = 0
     sumHumd = 0
     sumPres = 0
+    sumCpuTemp = 0
+    
+    # Random stuff for the SenseHAT RGBs
     reverse=bool(getrandbits(1))
     fillColor = [randint(0,255),randint(0,255),randint(0,255)]
     emptyColor = [randint(0,255),randint(0,255),randint(0,255)]
+
+    # Now take a number of samples of the different sensors
 
     for t in range(samples):
         sensedTemp = sensor.get_temperature()
@@ -51,90 +53,70 @@ def sampleEnvironment(sensor, samples, decimalPoints, delaySeconds):
         array2 = array[1].split("'")
         
         cpu_tempc = float(array2[0])
-        cpu_tempc = float("{0:.2f}".format(cpu_tempc))
-
-        temp_calibrated_c = sensedTemp - ((cpu_tempc - sensedTemp)/5.466)
         
-        sumTemp += sensor.get_temperature()
+        sumCpuTemp += cpu_tempc
+        
+        cpu_tempc = float("{0:.2f}".format(cpu_tempc))
+        temp_calibrated_c = sensedTemp - ((cpu_tempc - sensedTemp)/54.66)
+        
+        sumTemp += temp_calibrated_c
         sumHumd += sensor.get_humidity()
         sumPres += sensor.get_pressure()
 
         pixelProgress((t+1),samples,fillColor,emptyColor,reverse)
 
-        sleep(delaySeconds)
+    # Return the average
 
     return {
             "temperature" : str(round(sumTemp / samples, decimalPoints)),
             "humidity" : str(round(sumHumd / samples, decimalPoints)),
-            "pressure" : str(round(sumPres / samples, decimalPoints))}
+            "pressure" : str(round(sumPres / samples, decimalPoints)),
+            "hostTemperature" : str(round(sumCpuTemp / samples, decimalPoints))}
 
-def sampleAverages(sensor):
+
+def sampleAverages(sensor, hostname):
     # Get a good number of readings from the senseHAT
 
-    sample = sampleEnvironment(sensor,10,4,0.1)
+    sample = sampleEnvironment(sensor,50,4)
 
-    # Add to the readings
+    # Add to the readings for this host
 
     return({
+        'hostname' : hostname,
         'datetime': datetime.now().isoformat(),
         'temperature': sample["temperature"],
         'humidity': sample["humidity"],
-        'pressure': sample["pressure"]})
+        'pressure': sample["pressure"],
+        'hostTemperature': sample["hostTemperature"]})
 
 
 ######################################################################
 
-
-# outputFileFormat = "shed_%Y%m%d%H%M"
-
-# Connect to the senseHat
+# Connect to the senseHat, get the hostname, and connect to Kafka
 
 sense = SenseHat()
-
-# Connect to Kafka
-
+hostname = socket.gethostname()
 print ('Connecting to Kafka.')
 producer = KafkaProducer(bootstrap_servers='brenda.local:9092')
 print ('Done.')
 
-# Collect some data until the joystick is activated
+# Now collect some average data until the joystick is activated
 
 try:
 
     anyJoystickEvents = (len(sense.stick.get_events()) != 0)
 
     while not anyJoystickEvents:
+        averageReadings = sampleAverages(sense, hostname)
+        producer.send('sensehat',json.dumps(averageReadings))
+        anyJoystickEvents = (len(sense.stick.get_events()) != 0)
 
-        #outputFile = datetime.now().strftime(outputFileFormat) + ".json"
-        #oldOutputFile = outputFile
-        #data = {}
-        #data["readings"] = []
-
-        # while (outputFile == oldOutputFile) and (not anyJoystickEvents):
-            averageReadings = sampleAverages(sense)
-            producer.send('sensehat',json.dumps(averageReadings))
-
-            # Given the readings, what is the correct name for the output file?
-            # Because I cast the datetime above to make sure the JSON serializes, this bit doesn't work...
-            # outputFile = averageReadings["datetime"].strftime(outputFileFormat) + ".json"
-
-            #if outputFile != oldOutputFile:
-                # print ("Writing to new file...", outputFile)
-                # writeToJson(data, outputFile)
-                #oldOutputFile = outputFile
-                #data["readings"] = []
-
-            #data["readings"].append(averageReadings)
-
-            anyJoystickEvents = (len(sense.stick.get_events()) != 0)
-
-    #writeToJson(data, outputFile)
     producer.flush()
 
 except Exception as error:
     print("Exception Thrown\n", repr(error))
 
-# Tidy up
+# All done - tidy up
 
 sense.clear(0,0,0)
 producer.close();
