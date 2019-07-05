fdd8247cde7fac75176903af492af42368c068af
Peter Martshall <peter.marshall@uriam.com>
2019-07-05 16:32:18 +0100

    Fixed configparser
    Signed-off-by: Peter Martshall <peter.marshall@uriam.com>

--- kafkatest.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaProducer
-
-producer = KafkaProducer(bootstrap_servers='brenda.local:9095')
-
-for _ in range(100):
-	producer.send('sensehat', b'some_message_bytes')
-	print('sent ', _)
--- senseIt.py
+++ senseIt.py
@@ -1,106 +1,54 @@
-from sense_hat import SenseHat
-from random import getrandbits
-from random import randint
-from datetime import datetime
-from kafka import KafkaProducer
-import subprocess
 import json
 import socket
+import subprocess
+from datetime import datetime
 from backports import configparser
+from kafka import KafkaProducer
+from sense_hat import SenseHat
 
 
-def pixelProgress(complete, total, fill, empty, reverse):
-    if (complete > total):
-        raise ValueError("pixelProgress : completed exceeds the total")
+def take_reading():
+    decimal_points = 4
 
-    pixelsPerPoint = (64 / total)
-    progress = int(round(complete * pixelsPerPoint))
-    remaining = 64 - progress
+    cpu_temp = subprocess.check_output("vcgencmd measure_temp", shell=True)
+    array = cpu_temp.split("=")
+    array2 = array[1].split("'")
+    cpu_temp_c = float(array2[0])
+    cpu_temp_c = float("{0:.2f}".format(cpu_temp_c))
+    return_host_temp = cpu_temp_c
 
-    if reverse:
-        M = (remaining * [empty]) + (progress * [fill])
+    sensed_temp = sense.get_temperature()
+    if  config_calibrate = 'true':
+        return_temp = sensed_temp - ((cpu_temp_c - sensed_temp) / 54.66)
     else:
-        M = (progress * [fill]) + (remaining * [empty])
-
-    if (len(M) > 64):
-        raise ValueError("pixelProgress : catastrophic miscalculation of the necessary pixel string length!")
-    sense.set_pixels(M)
-
-
-def sampleEnvironment(sensor, samples, decimalPoints):
-    # Variable setup
-    sumTemp = 0
-    sumHumd = 0
-    sumPres = 0
-    sumCpuTemp = 0
-
-    # Random stuff for the SenseHAT RGBs
-    reverse = (bool(getrandbits(1)))
-    fillColor = [randint(0, 255), randint(0, 255), randint(0, 255)]
-    emptyColor = [randint(0, 255), randint(0, 255), randint(0, 255)]
-
-    # Now take a number of samples of the different sensors
-
-    for t in range(samples):
-        sensedTemp = sensor.get_temperature()
-
-        cpu_temp = subprocess.check_output("vcgencmd measure_temp", shell=True)
-        array = cpu_temp.split("=")
-        array2 = array[1].split("'")
-
-        cpu_tempc = float(array2[0])
-
-        sumCpuTemp += cpu_tempc
-
-        cpu_tempc = float("{0:.2f}".format(cpu_tempc))
-        temp_calibrated_c = sensedTemp - ((cpu_tempc - sensedTemp) / 54.66)
-
-        sumTemp += temp_calibrated_c
-        sumHumd += sensor.get_humidity()
-        sumPres += sensor.get_pressure()
-
-        pixelProgress((t + 1), samples, fillColor, emptyColor, reverse)
-
-    # Return the average
-
-    return {
-        "temperature": str(round(sumTemp / samples, decimalPoints)),
-        "humidity": str(round(sumHumd / samples, decimalPoints)),
-        "pressure": str(round(sumPres / samples, decimalPoints)),
-        "hostTemperature": str(round(sumCpuTemp / samples, decimalPoints))}
-
-
-def sampleAverages(sensor, hostname):
-    # Get a good number of readings from the senseHAT
-
-    sample = sampleEnvironment(sensor, 50, 4)
-
-    # Add to the readings for this host
+        return_temp = sensed_temp
 
     return ({
-        'hostname': hostname,
-        'datetime': datetime.now().isoformat(),
-        'temperature': sample["temperature"],
-        'humidity': sample["humidity"],
-        'pressure': sample["pressure"],
-        'hostTemperature': sample["hostTemperature"]})
-
+        'hostname': config_hostname,
+        'datetime': datetime.utcnow().isoformat(),
+        'temperature': str(round(return_temp, decimal_points)),
+        'humidity': str(round(sense.get_humidity(), decimal_points)),
+        'pressure': str(round(sense.get_pressure(), decimal_points)),
+        'hostTemperature': str(round(return_host_temp, decimal_points))})
 
 ######################################################################
-
-
 # Get configuration
 config = configparser.ConfigParser()
 config.read('senseIt.ini')
-configKafka = config['Kafka']['bootstrapServer'] + ':' + config['Kafka']['bootstrapServerPort']
-print(configKafka)
+config_kafka = config['Kafka']['bootstrapServer'] + ':' + config['Kafka']['bootstrapServerPort']
+config_hostname = config['PyPiImply']['hostname']
+config_topic = config['Kafka']['topic']
+config_calibrate = config['PyPiImply']['calibrate_temp']
+print(config_kafka)
+
 # Connect to the senseHat, get the hostname, and connect to Kafka
 
 sense = SenseHat()
+sense.clear(0, 0, 0)
 hostname = socket.gethostname()
 print('Connecting to Kafka.')
-producer = KafkaProducer(bootstrap_servers=configKafka)
-print('Done.')
+producer = KafkaProducer(bootstrap_servers=config_kafka)
+print('Collecting sensor readings.  Move the joystick to stop.')
 
 # Now collect some average data until the joystick is activated
 
@@ -109,8 +57,7 @@ try:
     anyJoystickEvents = (len(sense.stick.get_events()) != 0)
 
     while not anyJoystickEvents:
-        averageReadings = sampleAverages(sense, hostname)
-        producer.send('sensehat', json.dumps(averageReadings))
+        producer.send(config_topic, json.dumps(take_reading()))
         anyJoystickEvents = (len(sense.stick.get_events()) != 0)
 
     producer.flush()
@@ -121,6 +68,6 @@ except Exception as error:
     # All done - tidy up
 
 sense.clear(0, 0, 0)
-producer.close();
+producer.close()
 
 print("Finished")
